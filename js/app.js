9b933e83fa61be0398d31fdb957152a375f4ae85
Alesso  Ferrari <orangeferrari212@gmail.com>
2022-09-01 00:22:09 -0400

    5 files on vs code but images are not rendering

--- js/app.js
+++ js/app.js
@@ -1,21 +1,60 @@
 /* write js here  */
+console.log("LOG #1")
 
-function searchMarvelCharacters (searchTerm, limit=100, offset=1) {
-  const apiBaseURL = "http://gateway.marvel.com/v1/public"
-  const endpoint = "/characters?"
+document.body.addEventListener("keydown", event => {
+    console.log("LOG #2")
+    console.log(event.key)
+})
 
-  const timestamp = Date.now()
-  const publicKey = "6dd581e143d4eb3659beaa6938688fbe"
-  const privateKey = "ae2d0ea904b0dab43c426401467307d66e4e3d03"
-  const hash = md5(timestamp + privateKey + publicKey)
+console.log("LOG #3")
 
-  // const queryParameters = `ts=${timestamp}&apikey=${publicKey}&hash=${hash}&nameStartsWith=${searchTerm}&limit=${limit}&offset=${offset}`
-  const queryParameters = new URLSearchParams({
-      nameStartsWith: searchTerm,
-      limit: limit,
-      offset: offset,
-      ts: timestamp,
-      apikey: publicKey,
-      hash: hash,
-  })
+function searchMarvelCharacter (searchTerm, limit=100, offset=1) {
+    const apiBaseURL = "http://gateway.marvel.com/v1/public"
+    const endpoint = "/characters?"
+
+    const timestamp = Date.now()
+    const publicKey = "b9fc22f1180ad898d71fd0aefbf2d6c7"
+    const privateKey = "7601e2197395f07918ed83b4fcb768bd5451c9b7"
+    const hash = md5(timestamp + privateKey + publicKey)
+    // const queryParameters = `ts=${timestamp}&apikey=${publicKey}&hash=${hash}&nameStartsWith=${searchTerm}&limit=${limit}&offset=${offset}`
+    const queryParameters = new URLSearchParams({
+        nameStartsWith: searchTerm,
+        limit: limit,
+        offset: offset,
+        ts: timestamp,
+        apikey: publicKey,
+        hash: hash,
+    })
+    const url = apiBaseURL + endpoint + queryParameters.toString()
+    return fetch(url)
+        .then(response => response.json())
+        .then(body => {
+            const matchedCharacters = body.data.results
+            return matchedCharacters
+        })
+        // Any data you return from a Promise will be automatically wrapped in a new Promise()!
 }
+document
+    .querySelector("#search-form")
+    .addEventListener("submit", event => {
+        event.preventDefault()
+        const form = event.target
+        const searchInputElement = form.elements.search
+        
+        handleSearchInput(searchInputElement.value)
+    })
+function handleSearchInput (searchTerm) {
+    searchMarvelCharacters(searchTerm)
+        .then(characters => {
+            console.log(characters)
+            const charactersWithValidImages = characters
+                .filter(outInvalidImages)
+            renderGalleryView(charactersWithValidImages)
+        })
+}
+function outInvalidImages (character) {
+    const path = character.thumbnail.path
+    return path.includes("image_not_available") === false
+        && path.includes("4c002e0305708") === false
+}
+handleSearchInput("spider")
